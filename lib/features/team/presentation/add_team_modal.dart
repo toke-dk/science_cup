01d71c95aa11147b5d30dd04e7f6d5ed21aff78a
Toke Friis <tokefriis@gmail.com>
2026-06-22 07:09:14 +0200

    the team modeal now fetches the programs

--- lib/features/team/presentation/add_team_modal.dart
+++ lib/features/team/presentation/add_team_modal.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:science_cup_app/features/program/state/program_notifier.dart';
+import 'package:science_cup_app/providers/data_state.dart';
 
 import '../../shared/presentation/create_entity_modal.dart';
 
@@ -7,16 +10,29 @@ class AddTeamModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final programState = context.read<ProgramNotifier>().state;
 
-
-    return CreateEntityModal(
-      title: 'Opret gruppe',
-      fields: [
-        FieldConfig.select(key: "program", label: "Studie", options: <String>["Et studie her"], optionLabel: (program) => program, validator: (v) => v == null ? 'Vælg studie' : null),
-      ],
-      onSubmit: (data) async {
-        // data['name'] er en trimmed String
-        // await context.read<TeamNotifier>().createTeam(name: data['name']);
+    return programState.when(
+      initial: () => CircularProgressIndicator(),
+      loading: () => CircularProgressIndicator(),
+      error: (e) => Text("Fejl ved indlæsning af programmer: $e"),
+      loaded: (programs) {
+        return CreateEntityModal(
+          title: 'Opret gruppe',
+          fields: [
+            FieldConfig.select(
+              key: "program",
+              label: "Studie",
+              options: programs,
+              optionLabel: (program) => program.name ?? "?",
+              validator: (v) => v == null ? 'Vælg studie' : null,
+            ),
+          ],
+          onSubmit: (data) async {
+            // data['name'] er en trimmed String
+            // await context.read<TeamNotifier>().createTeam(name: data['name']);
+          },
+        );
       },
     );
   }
