15116d87ec215f9e2796fafebf7a71c2f2029af8
Toke Friis <tokefriis@gmail.com>
2026-06-24 13:37:32 +0200

    fixed the scaffold problm by replacing with modal bottom sheet

--- lib/features/team/presentation/add_team_modal.dart
+++ lib/features/team/presentation/add_team_modal.dart
@@ -47,65 +47,79 @@ class _AddTeamModalState extends ConsumerState<AddTeamModal> {
     // Begge er klar data er garanteret!
     final programs = programState.value!;
     final contacts = contactsState.value!;
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: CreateEntityModal(
-        title: 'Opret Hold',
-        fields: [
-          SelectFieldConfig(
-            key: "program",
-            label: "Studie",
-            options: programs,
-            optionLabel: (program) => program.name ?? "?",
-            validator: (v) => v == null ? 'Vælg studie' : null,
-          ),
-          OpenBottomSheetFieldConfig(
-            builder: (context) => SaveProgramModal(),
-            icon: Icon(Icons.add),
-            label: "Nyt studie",
-          ),
-          DividerFieldConfig(height: 32, thickness: 1),
-          TextConfig(label: 'Kontakter'),
-          WidgetFieldConfig(
-            child: DropdownSearch<Contact>.multiSelection(
-              items: (filter, s) => contacts
-                  .where((e) => e.name?.contains(filter) ?? false)
-                  .toList(),
-              compareFn: (item, selectedItem) => item.id == selectedItem.id,
-              popupProps: MultiSelectionPopupProps.modalBottomSheet(
-                showSearchBox: true,
-                itemBuilder: (context, item, isDisabled, isSelected) {
-                  return ListTile(
-                    title: Text(item.name ?? "?"),
-                    subtitle: Text(item.phone ?? "?"),
-                  );
-                },
-                suggestionsProps: SuggestionsProps(
-                  showSuggestions: true,
-                  items: (us) {
-                    return us
-                        .where((e) => e.name?.contains("Mrs") ?? false)
-                        .toList();
-                  },
-                  itemProps: SuggestedItemProps(),
+    return CreateEntityModal(
+      title: 'Opret Hold',
+      fields: [
+        SelectFieldConfig(
+          key: "program",
+          label: "Studie",
+          options: programs,
+          optionLabel: (program) => program.name ?? "?",
+          validator: (v) => v == null ? 'Vælg studie' : null,
+        ),
+        OpenBottomSheetFieldConfig(
+          builder: (context) => SaveProgramModal(),
+          icon: Icon(Icons.add),
+          label: "Nyt studie",
+        ),
+        DividerFieldConfig(height: 32, thickness: 1),
+        TextConfig(label: 'Kontakter'),
+        WidgetFieldConfig(
+          child: DropdownSearch<Contact>.multiSelection(
+            items: (filter, s) => contacts
+                .where((e) => e.name?.contains(filter) ?? false)
+                .toList(),
+            compareFn: (item, selectedItem) => item.id == selectedItem.id,
+            popupProps: MultiSelectionPopupProps.modalBottomSheet(
+              containerBuilder: (ctx, popupWidget) {
+                return Padding(
+                  padding: const EdgeInsets.all(
+                    8.0,
+                  ), // Padding hele vejen rundt
+                  child: popupWidget,
+                );
+              },
+              applyButtonProps: ButtonProps(
+                text: 'Tilføj',
+                icon: Icon(Icons.add),
+              ),
+              searchFieldProps: TextFieldProps(
+                decoration: InputDecoration(
+                  labelText: 'Søg kontakter',
+                  border: OutlineInputBorder(),
                 ),
+              ),
+
+              showSearchBox: true,
+              itemBuilder: (context, item, isDisabled, isSelected) {
+                return ListTile(
+                  title: Text(item.name ?? "?"),
+                  subtitle: Text(item.phone ?? "?"),
+                );
+              },
+              suggestionsProps: SuggestionsProps(
+                showSuggestions: true,
+                items: (us) {
+                  return us
+                      .where((e) => e.name?.contains("Mrs") ?? false)
+                      .toList();
+                },
+                itemProps: SuggestedItemProps(),
               ),
             ),
           ),
-        ],
-        onSubmit: (data) async {
-          final seasonId = ref.read(currentSeasonProvider)?.id;
-          if (seasonId == null) {
-            ScaffoldMessenger.of(
-              context,
-            ).showSnackBar(SnackBar(content: Text("Ingen sæson valgt")));
-            return;
-          }
-          ref
-              .read(teamProvider(seasonId).notifier)
-              .createTeam(name: "Nyt Hold");
-        },
-      ),
+        ),
+      ],
+      onSubmit: (data) async {
+        final seasonId = ref.read(currentSeasonProvider)?.id;
+        if (seasonId == null) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text("Ingen sæson valgt")));
+          return;
+        }
+        ref.read(teamProvider(seasonId).notifier).createTeam(name: "Nyt Hold");
+      },
     );
   }
 }
